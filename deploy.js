071a4a5fc45045c8f56402d04c896603f13389ec
Shaheryar Akram <38584504+Sherry-94@users.noreply.github.com>
2018-10-28 18:53:26 +0500

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -1,3 +1,26 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+  'opera hen butter claw sport sheriff quantum initial steel teach sample fine',
+  'https://rinkeby.infura.io/orDImgKRzwNrVCDrAk5Q'
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: "0x" +bytecode, arguments: ['0xca35b7d915458ef540ade6068dfe2f44e8fa733c'] })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+};
+deploy();
+
 // const HDWalletProvider = require('truffle-hdwallet-provider');
 // const Web3 = require('web3');
 // const { interface, bytecode } = require('./compile');
@@ -30,27 +53,3 @@
 //   console.log('Contract deployed to', inbox.options.address);
 // };
 // deploy();
-
-
-const HDWalletProvider = require('truffle-hdwallet-provider');
-const Web3 = require('web3');
-const { interface, bytecode } = require('./compile');
-
-const provider = new HDWalletProvider(
-  'opera hen butter claw sport sheriff quantum initial steel teach sample fine',
-  'https://rinkeby.infura.io/orDImgKRzwNrVCDrAk5Q'
-);
-const web3 = new Web3(provider);
-
-const deploy = async () => {
-  const accounts = await web3.eth.getAccounts();
-
-  console.log('Attempting to deploy from account', accounts[0]);
-
-  const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: "0x" +bytecode, arguments: ['0xca35b7d915458ef540ade6068dfe2f44e8fa733c'] })
-    .send({ gas: '1000000', from: accounts[0] });
-
-  console.log('Contract deployed to', result.options.address);
-};
-deploy();
